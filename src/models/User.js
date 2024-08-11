0a32e2f5e8df3be07f6726a9585b13d97ddcbabb
abelzavaleta <abelzavaleta08@gmail.com>
2024-08-11 16:28:29 -0400

    Users: users model

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    default: 'admin',
+  },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
