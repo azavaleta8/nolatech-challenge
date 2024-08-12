8ef62e50bdbe84a12946c3534d9b8b2ca2caf6d4
abelzavaleta <abelzavaleta08@gmail.com>
2024-08-12 03:04:38 -0400

    Question: model

---
+++ src/models/Question.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  options: [{
+    type: String,
+    required: true
+  }],
+  correctAnswer: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, { timestamps: true });
+
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
